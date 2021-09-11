9509c4b9da2013b7a27a05d2975eca6562d78648
Carlos Colombo <csc7@hw.ac.uk>
2021-09-11 19:59:52 +0000

    Prepare run.py with PEP8, still without final comments

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from functions import *
 
+
 # Main part of program, calling all functions
 def main(run_program):
     """
@@ -12,68 +13,73 @@ def main(run_program):
         if (run_program == "G" or run_program == "g"):
             data_source = 'Google Drive'
             data_loaded = load_sheets_from_Google_Drive()
-            if (data_loaded == False):
+            if (not data_loaded):
                 break
             qc_data = validate_data_from_Google(data_loaded)
-            
 
         if (run_program == "L" or run_program == "l"):
             data_source = 'Local Drive'
             data_loaded = load_sheets_locally()
-            if (data_loaded == False):
+            if (not data_loaded):
                 break
             qc_data = validate_data_locally(data_loaded)
-            
 
-        # 2nd Function: Validation
-        #qc_data = validate_data(data_loaded[slice(0, 5)])
-
-        #qc_data, QCSDA_EXCEL_FILE = validate_data(data_loaded)
-
-        # 3rd Function: Get daily acquisition numbers/totals
-        daily_amounts, warning_message = get_daily_amounts (qc_data)
+        # Get daily acquisition numbers/totals
+        daily_amounts, warning_message = get_daily_amounts(qc_data)
         if (warning_message != []):
             print("WARNING:")
-            print(f"Acording to daily report, {str(daily_amounts['daily_report']['date'])[0:10]} daily production is {daily_amounts['daily_report']['daily_prod']} VPs.")
-            print(f"The amount of data in the following file/s does/do not match the daily production:\n{warning_message}\n")
-            print("If you continue you will get statistics for an incomplete data set.")
-            print('Press "Y" + "enter" to continue or other key + "enter" to close the program.\n')
+            print("Acording to daily report, " +
+                  f"{str(daily_amounts['daily_report']['date'])[0:10]} "
+                  "daily production is " +
+                  f"{daily_amounts['daily_report']['daily_prod']} VPs.")
+            print("The amount of data in the following file/s does/do not " +
+                  f"match the daily production:\n{warning_message}\n")
+            print("If you continue you will get statistics " +
+                  "for an incomplete data set.")
+            print('Press "Y" + "enter" to continue or other key + "enter" ' +
+                  'to close the program.\n')
             cont_option = input('Select option: \n')
             if (cont_option == "Y" or cont_option == "y"):
                 pass
             else:
                 print("Program closed.")
-                break               
-        print(f"\n{str(daily_amounts['daily_report']['date'])[0:10]} daily production: {daily_amounts['daily_report']['daily_prod']},")
-        print(f"with {daily_amounts['daily_report']['daily_layout']} planted geophones and {daily_amounts['daily_report']['daily_pick_up']} picked up.\n")
-        
+                break
+        print(f"\n{str(daily_amounts['daily_report']['date'])[0:10]} daily " +
+              f"production: {daily_amounts['daily_report']['daily_prod']}, " +
+              "with ")
+        print(f"{daily_amounts['daily_report']['daily_layout']} " +
+              "planted geophones and " +
+              f"{daily_amounts['daily_report']['daily_pick_up']} picked up.\n")
+
         # Data OK, present menu options to user
         while(True):
-        
             print('Select one option below and press the number + "enter":')
             print("1 - Compute Points to reacquire")
             print("2 - Visualize data")
             print("3 - Update QCSDA Spreadsheet with points to reacquire")
             print("Other key - Back to main menu\n")
             answer = input("Select option: \n")
-        
+
             if (answer == '1'):
                 points_to_reaquire = get_points_to_reaquire(daily_amounts)
             elif (answer == '2'):
                 try:
                     visualize_data(daily_amounts, points_to_reaquire)
                 except UnboundLocalError:
-                    print("\nWARNING!\nPoints to be reaquired not computed, passing existing and daily data only!\n")
+                    print("\nWARNING!\nPoints to be reaquired not computed,\
+                        passing existing and daily data only!\n")
                     visualize_data(qc_data)
             elif (answer == '3'):
                 try:
-                    update_qcsda(points_to_reaquire, daily_amounts['daily_report']['date'], data_source)
+                    update_qcsda(points_to_reaquire,
+                                 daily_amounts['daily_report']['date'],
+                                 data_source)
                 except UnboundLocalError:
-                    print("\nPlease get points to reacquire first (run option 1) before updating\n")
+                    print("\nPlease get points to reacquire first. ")
+                    print ("Run option 1 before updating.\n")
                     continue
             else:
                 break
-
 
         # End of program, ask to restart or close
 
@@ -82,7 +88,7 @@ def main(run_program):
         print('Press any other key + "enter" to close the program.\n')
         run_again = input('Select option: \n')
         if (run_again == "G" or run_again == "g" or
-            run_again == "L" or run_again == "l"):
+                run_again == "L" or run_again == "l"):
             run_program = run_again
         else:
             print("Program closed.")
@@ -91,7 +97,7 @@ def main(run_program):
 
 print("")
 print("-----------------------------------------------------------------")
-print("Welcome to your Daily Quality Control of Seismic Dcquisition Data")
+print("Welcome to your Daily Quality Control of Seismic Data Acquisition")
 print("-----------------------------------------------------------------")
 print("")
 print("Make sure you have the following sheets in the working folder:")
@@ -111,7 +117,7 @@ print('Press any other key + "enter" to close the program.\n')
 run_program = input('Select option: \n')
 
 if (run_program == "G" or run_program == "g" or
-    run_program == "L" or run_program == "l"):
+        run_program == "L" or run_program == "l"):
     main(run_program)
 else:
     print("Program closed.")
